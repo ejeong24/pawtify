80aac7ec546480d98e3ec847b76e170aafc31abc
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-05-15 18:26:54 -0500

    internal navigation

--- src/components/Artist/Artist.jsx
+++ src/components/Artist/Artist.jsx
@@ -105,22 +105,9 @@ function Artist() {
           </div>
         </div>
       </header>
-      <article className="mt-6 pt-6 border-t-[1px] border-opacity-30 border-solid border-slate-600">
-        <div className="flex justify-center items-center gap-4">
-          <h1 className="text-center text-3xl">Album Title</h1>
-          <span>Released on: </span>
-        </div>
-        <div className="flex w-full">
-          <div className="text-[18px] mr-6">Like</div>
-          <div className="ml-6 border-b-[1px] border-solid border-slate-500 border-opacity-40 pb-2 flex w-full">
-            <span className="w-full text-[18px]">Track</span>
-
-            <span className="justify-end text-[18px]">Length</span>
-          </div>
-        </div>
-        {/* {trackListing} */}
+      <article className="mt-6 pt-2 border-solid border-slate-600">
+        {albumsAndTracksList}
       </article>
-      {albumsAndTracksList}
     </article>
   );
 }
--- src/components/Playlist/Playlists.jsx
+++ src/components/Playlist/Playlists.jsx
@@ -1,63 +1,20 @@
 import React, {useEffect, useState} from "react";
-import {useLoaderData} from "react-router-dom";
+import {useLoaderData, Link} from "react-router-dom";
 import Tracks from "../Track/Tracks";
 
-export async function loader({params}) {
-  //   const tracks = await getOne(
-  //     "playlistTracks",
-  //     params.id,
-  //     "/tracks?offset=0&limit=20",
-  //   );
-  //   return {tracks};
-}
-
 function Playlists({playlist}) {
-  const {tracks} = useLoaderData();
-
-  const [trackList, setTrackList] = useState({});
-  let displayTracks = [];
-  let showDisplayTracks;
-  useEffect(() => {
-    //regularGET
-    let access_token = localStorage.getItem("access_token");
-    fetch(`${playlist.tracks.href}`, {
-      method: "GET",
-      headers: {Authorization: `Bearer ${access_token}`},
-    })
-      .then(resp => resp.json())
-      .then(trackList => setTrackList(trackList))
-      .catch(error => console.log("error", error.message));
-  }, []);
-  if (trackList.items) {
-    for (let i = 0; i < 10; i++) {
-      displayTracks.push(trackList.items[i]);
-    }
-    showDisplayTracks = displayTracks.map(track => (
-      <Tracks
-        key={track.track.id !== null ? track.track.id : index}
-        track={track.track}
-      />
-    ));
-  }
-
   return (
-    <div className="card max-w-[480px] bg-base-100 shadow-xl">
-      <figure>
-        <img src={playlist.images[0].url} alt={playlist.name} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{playlist.name}</h2>
-        <h4>{playlist.description}</h4>
-        <div
-          tabIndex={0}
-          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mx-0 px-0">
-          <div className="collapse-title text-xl font-medium">Tracks</div>
-          <div className="collapse-content mx-0 px-0">
-            <ol>{showDisplayTracks ? showDisplayTracks : "Nothing Here"}</ol>
-          </div>
+    <Link to={`../playlists/${playlist.id}`}>
+      <div className="card max-w-[480px] bg-base-100 shadow-xl hover:transition-transform hover:scale-105 hover:ease-in-out hover:duration-300">
+        <figure>
+          <img src={playlist.images[0].url} alt={playlist.name} width="360px" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{playlist.name}</h2>
+          <h4>{playlist.description}</h4>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
