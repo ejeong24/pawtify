10eeb20483ec403d3d6862bd9bceec7a6e0e914b
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-05-15 20:51:42 -0500

    1021 5-16

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,37 @@
+import React, {useContext, useState} from "react";
+import {getProfiles} from "../Rover";
+import {useLoaderData} from "react-router-dom";
+export async function loader() {
+  const profiles = await getProfiles();
+  return {profiles};
+}
+function Users() {
+  const {profiles} = useLoaderData();
+  return <div>Users</div>;
+}
+
+export default Users;
+
+export function UserCard(user) {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+}
